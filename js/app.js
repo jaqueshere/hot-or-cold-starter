b41f5004e371ccb5d7322fba045a7348b65744d1
Jake Gillis <jaques.gillis@gmail.com>
2015-11-22 12:15:24 -0500

    Added basic functionality

--- js/app.js
+++ js/app.js
@@ -12,6 +12,32 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	NewGame();
+
+    $(".new").click(NewGame());
+
+    $("#guessButton").click( 
+      function(e) {
+        e.preventDefault();
+        guess = $("#userGuess").val();
+        guessCount++;
+        temperature = Math.abs(myNumber - guess);
+        tempDisplay = "";
+        if (temperature > 20)
+          tempDisplay = "cold";
+        else
+          tempDisplay = "hot";
+        $("#feedback").text(tempDisplay);
+        $("#guessList").append("<li>" + guess + "</li>");
+        $("#count").text(guessCount);
+    });
+
+    function NewGame() {
+      guessCount = 0;
+      myNumber = Math.floor((Math.random()*100) + 1);
+      lastGuess = 0;
+    }
+
 });
 
 
