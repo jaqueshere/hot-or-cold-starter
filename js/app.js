a9874da7a978f3d08499c8a559371922b9ee6634
Jake Gillis <jaques.gillis@gmail.com>
2015-11-22 13:56:55 -0500

    Added verification code, and organized JS

--- js/app.js
+++ js/app.js
@@ -20,31 +20,12 @@ $(document).ready(function(){
       function(e) {
         e.preventDefault();
         guess = $("#userGuess").val();
+        check(guess);
         var oldTemp = Math.abs(myNumber - lastGuess);
         guessCount++;
         var temperature = Math.abs(myNumber - guess);
         tempDisplay = "";
-
-        if (guess == myNumber) {
-          tempDisplay = "just right!";
-        }
-
-        else if (firstGuess && (temperature > 20)) {
-          tempDisplay = "cold";
-        }
-
-        else if (firstGuess && (temperature < 20)) {
-          tempDisplay = "hot";
-        }
-
-        else if (temperature > oldTemp) {
-          tempDisplay = "colder";
-        }
-
-        else {
-          tempDisplay = "hotter";
-        }
-
+        feedback(guess, oldTemp, temperature);
         $("#feedback").text(tempDisplay);
         $("#guessList").append("<li>" + guess + "</li>");
         $("#count").text(guessCount);
@@ -55,14 +36,47 @@ $(document).ready(function(){
 
 });
 
-    function NewGame() {
-      $("#feedback").text("Make your Guess!");
-      $("#guessList").empty();
-      $("#userGuess").val("");
-      guessCount = 0;
-      $("#count").text(guessCount);
-      myNumber = Math.floor((Math.random()*100) + 1);
-      lastGuess = 0;
-      firstGuess = true;
-      console.log("hello");
-    }
+function NewGame() {
+  $("#feedback").text("Make your Guess!");
+  $("#guessList").empty();
+  $("#userGuess").val("");
+  guessCount = 0;
+  $("#count").text(guessCount);
+  pickNumber();
+  lastGuess = 0;
+  firstGuess = true;
+  console.log("hello");
+}
+
+function pickNumber() {
+  myNumber = Math.floor((Math.random()*100) + 1);
+}
+
+function check(number) {
+  if ((number > 100) || (number < 1)) {
+    guess = prompt("Enter a number between 1 and 100: ");
+  }
+}
+
+function feedback(number, last, current) {
+  if (number == myNumber) {
+          tempDisplay = "just right!";
+        }
+
+  else if (firstGuess && (current > 20)) {
+          tempDisplay = "cold";
+        }
+
+  else if (firstGuess && (current < 20)) {
+          tempDisplay = "hot";
+        }
+
+  else if (current > last) {
+          tempDisplay = "colder";
+        }
+
+  else {
+          tempDisplay = "hotter";
+        }
+
+}
